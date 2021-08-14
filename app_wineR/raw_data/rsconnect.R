177760f9c5968d2af9eeeb9b057da38adcec8f14
jjwwll <87248915+jjwwll@users.noreply.github.com>
2021-08-14 10:52:43 -0400

    Add files via upload

---
+++ app_wineR/raw_data/rsconnect.R
@@ -0,0 +1,5 @@
+rsconnect::setAccountInfo(name='jaiwoolee92', token='5685BE51000215223A277489A8DFC2A3', secret='jWazgaa836UQVtjq4DtacUONqlr8o3+nyRi/tDpc')
+  library(rsconnect)
+  rsconnect::deployApp('~/Desktop/1. grad(PurdueBAIM)/Lectures/1.Summer_21/MGMT590(R)/project/app_wineR')
+y
+
